43fe7bf6742d1301286c66050829552dbeacb7c2
Rajesh Kumar Dwivedi <rajeshdwi@gmail.com>
2019-05-16 21:31:36 -0700

    more queue details

--- Linked-List/reverse-linked-list.py
+++ Linked-List/reverse-linked-list.py
@@ -7,24 +7,24 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        
+
     def append(self, value):
         if self.head is None:
             self.head = Node(value)
             return
-        
+
         node = self.head
         while node.next:
             node = node.next
 
         node.next = Node(value)
-        
+
     def __iter__(self):
         node = self.head
         while node:
             yield node.value
             node = node.next
-            
+
     def __repr__(self):
         return str([v for v in self])
 
@@ -38,40 +38,17 @@ def reverse(linked_list):
     Returns:
        obj: Reveresed Linked List
     """
-    
+
     # TODO: Write your function to reverse linked lists here
     if linked_list.head is None:
         return None
     new_llist = LinkedList()
-    next_node = None
     current_node = linked_list.head
     while current_node:
         next_node = current_node.next
         current_node.next = new_llist.head
         new_llist.head = current_node
         current_node = next_node
-    # if linked_list.head is None:
-    #     return None
-    # new_llist = LinkedList()
-    # currenrt_node = linked_list.head
-    # while currenrt_node:
-    #     node = Node(currenrt_node.value)
-    #     if new_llist.head is None:
-    #         new_llist.head = node
-    #     else:
-    #         node.next = new_llist.head
-    #         new_llist.head = node
-    #     currenrt_node = currenrt_node.next
-    # return new_llist
-    # for val in linked_list:
-    #     if new_llist.head is None:
-    #         new_llist.head = Node(val)
-    #     else:
-    #         node = Node(val)
-    #         node.next = new_llist.head
-    #         new_llist.head = node
-    # linked_list = new_llist
-    # return linked_list
     return new_llist
     pass
 
@@ -87,7 +64,7 @@ def reverse_class(linked_list):
     """
     new_list = LinkedList()
     node = linked_list.head
-    
+
     # A bit of a complex operation here. We want to take the
     # node from the original linked list and prepend it to 
     # the new linked list
@@ -95,14 +72,14 @@ def reverse_class(linked_list):
         old_head = new_list.head
         new_node = node
         node = node.next
-        
+
         new_node.next = old_head
         new_list.head = new_node
     return new_list
 
 
 llist = LinkedList()
-for value in [4,2,5,1,-3,0]:
+for value in [4, 2, 5, 1, -3, 0]:
     llist.append(value)
 
-print ("Pass" if (llist == reverse(llist)) else "Fail")
+print("Pass" if (llist == reverse(llist)) else "Fail")
---
+++ Queue/queue-with-python.py
@@ -0,0 +1,32 @@
+class Queue:
+    def __init__(self):
+        self.arr = []
+
+    def size(self):
+        return len(self.arr)
+
+    def enqueue(self, item):
+        self.arr.append(item)
+
+    def dequeue(self):
+        return self.arr.pop(0)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print("Pass" if (q.dequeue() == 2) else "Fail")
+print("Pass" if (q.dequeue() == 3) else "Fail")
+print("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print("Pass" if (q.size() == 1) else "Fail")
